0dad7995c0d4552e212930bf9591d7cd02950fa5
Fernando <33039864+tuxslack@users.noreply.github.com>
2024-11-16 19:49:25 -0300

    Update install-sddm-theme.sh

--- install-sddm-theme.sh
+++ install-sddm-theme.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
+
+clear
+
+which sed          || exit
+which sddm         || exit
+which sddm-greeter || exit
+
+
 NAME="delicious"
 DIR="/usr/share/sddm/themes/$NAME/"
 CFG="/etc/sddm.conf"
 
+
 function changeCurrentTheme {
     sudo sed -i "s/^Current=.*/Current=$NAME/" $CFG
-    echo "Current theme changed to $NAME"
+    echo -e "Tema atual alterado para $NAME"
 }
 
 function disableVirtualKeyboard {
     if ! grep -wq "InputMethod=" $CFG; then
-        echo -e "\nDo you want to disable the virtual on-screen keyboard in SDDM? Select yes if you have a physical keyboard"
+    
+        echo -e "\nVocê quer desabilitar o teclado virtual na tela no SDDM? Selecione sim se você tiver um teclado físico"
+        
         select sel in "Yes" "No"; do
             case $sel in
                 Yes ) 
                 if grep -q "^InputMethod=qtvirtualkeyboard" $CFG; then
+                
                     sudo sed -i "s/^InputMethod=qtvirtualkeyboard/InputMethod=/" $CFG;
-                    echo "Virtual keyboard disabled (modified InputMethod entry)";
+                    
+                    echo -e "Teclado virtual desabilitado (entrada InputMethod modificada)";
+                    
                 elif ! grep -q "^InputMethod=" $CFG; then
+                
                     sudo sed -i 's/^\[General\]/\[General\]\nInputMethod=/' $CFG;
-                    echo "Virtual keyboard disabled (created empty InputMethod entry)";
+                    
+                    echo -e "Teclado virtual desabilitado (criou uma entrada InputMethod vazia)";
                 fi
                 break;;
                 No ) break;;
@@ -30,7 +46,9 @@ function disableVirtualKeyboard {
 }
 
 function testTheme {
-    echo -e "\nDo you want to test the theme now?"
+
+    echo -e "\nQuer testar o tema agora?"
+    
     select sel in "Yes" "No"; do
         case $sel in
             Yes ) sddm-greeter --test-mode --theme $DIR; break;;
@@ -40,25 +58,37 @@ function testTheme {
 }
 
 function createConfig {
+
     sddm --example-config | sudo tee $CFG > /dev/null
-    echo "Configuration file created in $CFG"
+    echo -e "Arquivo de configuração criado em $CFG"
 }
 
 if sudo cp -R . $DIR; then
-    echo "Theme installed in $DIR"
+
+    echo -e "Tema instalado em $DIR"
+    
 else
-    echo "Errors occurred during installation, exiting"
+
+    echo -e "Ocorreram erros durante a instalação, saindo"
+    
     exit;
+    
 fi
 
+
 if [ ! -f $CFG ]; then
-    echo -e "\nSDDM configuration file $CFG does not exist, do you want to create it based on current configuration?"
+
+    echo -e "\nO arquivo de configuração $CFG do SDDM não existe. Você deseja criá-lo com base na configuração atual?"
+    
     select sel in "Yes" "No"; do
         case $sel in
             Yes ) createConfig; changeCurrentTheme; disableVirtualKeyboard; testTheme; break;;
-            No ) echo "Theme installed in $DIR but configuration not changed."; testTheme; break;;
+            No )  echo -e "Tema instalado em $DIR, mas a configuração não foi alterada."; testTheme; break;;
         esac
     done
 else
     changeCurrentTheme; disableVirtualKeyboard; testTheme;
 fi
+
+
+exit 0
